56dbe9e341452173c10982e14e38148e5e3aeaf8
Silvia <soliveramendez@gmail.com>
2017-11-08 13:39:19 -0300

    ejercicios

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+<script type="text/javascript">
+function mostrar(){
+document.getElementById('nuevoForm').style.display = 'block';}
+</script>
+
+<input type="button" value="Mostrar" onclick="mostrar()">
